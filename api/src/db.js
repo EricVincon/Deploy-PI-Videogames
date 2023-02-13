91142c2e0eb6d6cc0456831ed49304311f4e2611
EricVincon <vinconeric20@gmail.com>
2023-02-13 20:02:33 -0300

    Coding

--- api/src/db.js
+++ api/src/db.js
@@ -3,9 +3,9 @@ const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
 const {
-  DB_USER, DB_PASSWORD, DB_HOST,} = process.env; //variables de entorno, el archivo env no se sube a github, nuestras credenciales estan protegidas
+  DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME} = process.env; //variables de entorno, el archivo env no se sube a github, nuestras credenciales estan protegidas
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/videogames`, {
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
